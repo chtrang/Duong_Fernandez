1e6eaa82c79d557f07ab7f4eca24cc60b118057d
duong <duong.elisabeth@gmail.com>
2015-12-28 03:46:10 +0100

    Mise à jour config development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,4 +40,14 @@ Rails.application.configure do
 
   #For devise
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  # Mailer GMail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            'rubyonrails654@gmail.com',
+    password:             'RubyOnRails',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 end
