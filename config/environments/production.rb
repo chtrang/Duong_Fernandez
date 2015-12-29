5818399fdd3f5b36170e4509b2135a0ac64d1569
duong <duong.elisabeth@gmail.com>
2015-12-29 15:59:34 +0100

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,21 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  
+  #For paperclip
+  Paperclip.options[:command_path] = "/usr/bin/"
+  
+  # Raise an error on page load if there are pending migrations.
+  config.active_record.migration_error = :page_load
+
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  config.assets.debug = true
+
+  # Adds additional error checking when serving assets at runtime.
+  # Checks for improperly declared sprockets dependencies.
+  # Raises helpful error messages.
+  config.assets.raise_runtime_errors = true  
 
   # Code is not reloaded between requests.
   config.cache_classes = true
