e6fdbef92347d7ca5fe9d562ec68ae025d8479b3
duong <duong.elisabeth@gmail.com>
2015-12-31 16:45:56 +0100

    test

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,5 +6,9 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w['bootstrap.min.css']
-Rails.application.config.assets.precompile += %w( home.css )
-Rails.application.config.assets.precompile += %w( home.js )
+Rails.application.config.assets.precompile += %w( *.css.sass )
+Rails.application.config.assets.precompile += %w( *.css.scss )
+Rails.application.config.assets.precompile += %w( *.css )
+Rails.application.config.assets.precompile += %w( *.js )
+Rails.application.config.assets.precompile += %w( *.js.coffee )
+Rails.application.config.assets.precompile += %w( *.js.coffee.erb )
